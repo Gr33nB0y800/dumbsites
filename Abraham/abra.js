fd8aa6e79447fd7ebc7797e0cb6ccaa8b2be5624
Gr33nB0y800 <164310052+Gr33nB0y800@users.noreply.github.com>
2024-03-21 09:58:42 +0100

    Create abra.js ok

---
+++ Abraham/abra.js
@@ -0,0 +1,10 @@
+// Open the popup
+document.getElementById("contactBtn").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+});
+
+// Close the popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+
+}
