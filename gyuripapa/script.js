f8e9d7300089aed959614972d5bf53f6f12a6364
Gr33nB0y800 <164310052+Gr33nB0y800@users.noreply.github.com>
2024-05-13 15:18:22 +0200

    Update script.js

--- gyuripapa/script.js
+++ gyuripapa/script.js
@@ -1,3 +1,8 @@
-function orderCocktail() {
-    document.getElementById("order-status").innerText = "Your order has been placed! Enjoy Gyuri Papa's special cocktail!";
-}
+document.getElementById("order-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var address = document.getElementById("address").value;
+    var email = document.getElementById("email").value;
+    var quantity = document.getElementById("quantity").value;
+    document.getElementById("order-status").innerText = "Thank you, " + name + "! Your order for " + quantity + " special cocktails has been placed. We will deliver them to " + address + ". An email confirmation will be sent to " + email + ".";
+});
